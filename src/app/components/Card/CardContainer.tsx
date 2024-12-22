217eb2a08e0fac1a8ba9f88712bc5a18f781128c
Arthur Levy <arthurlbvieiraa@gmail.com>
2024-12-22 10:49:03 -0300

    Initial commit

---
+++ src/app/components/Card/CardContainer.tsx
@@ -0,0 +1,7 @@
+export function CardContainer ({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header className="w-full h-20 bg-emerald-500 flex items-center px-4">
+            <h1 className="text-4xl text-slate-100">Tech Store</h1>
+        </header>
+    );
+}
